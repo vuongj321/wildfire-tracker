616e8c70f1927a5e81bda1a36416cd98ea0d20d3
vuongj321 <jasonvuong04@gmail.com>
2026-04-20 14:41:07 -0500

    Implement react router for redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Map from "./components/Map";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Map />
-    </div>
+    <Routes>
+      <Route path="/" element={<Map />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     <Analytics />
   </StrictMode>,
 )
